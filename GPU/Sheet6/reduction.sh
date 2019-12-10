5be730f879dcb36456f7d632502747d93af122f2
Dkpingi <dkpingi@aol.com>
2019-12-10 17:16:14 +0100

    now actually final version

--- GPU/Sheet6/reduction.sh
+++ GPU/Sheet6/reduction.sh
@@ -4,15 +4,13 @@
 #SBATCH -w creek04
 
 rm greduction.txt
-for i in {7..16..1}
-do
-k=$((2**$i))
-bin/reduction -s $k -t 64
-done
-
-
 for j in {3..10..1}
 do
+for i in {8..17..1}
+do
+k=$((2**$i))
 l=$((2**$j))
-bin/reduction -s 8192 -t $l
+bin/reduction -s $k -t $l
 done
+done
+
